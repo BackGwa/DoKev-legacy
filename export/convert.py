e45cb7f2cc2d0839770efa6a5eed1fb53765c247
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-07-14 11:26:47 +0900

    소스 코드

--- export/convert.py
+++ export/convert.py
@@ -1,35 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-print("안녕, 세상아!") #출력
+# Dokev 220714.01부터 @  통하여 예외처리  진행합니다.
 
-
-변수_가 = input("입력해줘! >> ") #변수 선언 & 입력
-변수_가 = int(변수_가)  #자료형 변환
-변수_나 = 0 
-
-
-if(변수_가 == 10): #if문
-    print("값이 10이구나!")
-
-elif(변수_가 == 20): #elif문
-    print("값이 20이구나!")
-
-else: #else문
-    print("값  모르겠어...")
-
-
-while(변수_나 < 5): #while 문
-    print("반복 횟수 : {}".format(변수_나)) #format 문자
-    변수_나  += 1 #증감 연산
-    break #break문
-
-
-for 연산변수 in [1, 2, 3, 4, 5]:
-    print("연산변수 값 : {}".format(연산변수))
-
-
-def 더해버리자  (가_변수,  나_변수): #함수 선언
-    return 가_변수 + 나_변수 #값 반환
 
 print("안녕, 세상아!") #출력
 
@@ -46,7 +18,7 @@ elif(변수_가 == 20): #elif문
     print("값이 20이구나!")
 
 else: #else문
-    print("값  모르겠어...")
+    print("값을 모르겠어...")
 
 
 while(변수_나 < 5): #while 문
@@ -55,41 +27,12 @@ while(변수_나 < 5): #while 문
     break #break문
 
 
-for 연산변수 in [1, 2, 3, 4, 5]:
+for 연산변수 in [1, 2, 3, 4, 5]: #for 문
     print("연산변수 값 : {}".format(연산변수))
 
 
 def 더해버리자  (가_변수,  나_변수): #함수 선언
     return 가_변수 + 나_변수 #값 반환
 
-print("안녕, 세상아!") #출력
-
-
-변수_가 = input("입력해줘! >> ") #변수 선언 & 입력
-변수_가 = int(변수_가)  #자료형 변환
-변수_나 = 0 
-
-
-if(변수_가 == 10): #if문
-    print("값이 10이구나!")
-
-elif(변수_가 == 20): #elif문
-    print("값이 20이구나!")
-
-else: #else문
-    print("값  모르겠어...")
-
-
-while(변수_나 < 5): #while 문
-    print("반복 횟수 : {}".format(변수_나)) #format 문자
-    변수_나  += 1 #증감 연산
-    break #break문
-
-
-for 연산변수 in [1, 2, 3, 4, 5]:
-    print("연산변수 값 : {}".format(연산변수))
-
-
-def 더해버리자  (가_변수,  나_변수): #함수 선언
-    return 가_변수 + 나_변수 #값 반환
+print("{}".format(더해버리자(1, 2)))
 
