40333f9e619372a1df5b58cbccc4cf7dfce675af
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-07-15 10:37:12 +0900

    src

--- export/convert.py
+++ export/convert.py
@@ -2,39 +2,5 @@
 # -*- coding: utf-8 -*-
 # Dokev 220714.01부터 @  통하여 예외처리  진행합니다.
 
-
-import random 
-
-print("안녕, 세상아!") #print
-print("{}".format(random. randint(1, 100)))
-
-변수_가 = input("입력해줘! >> ") #변수 선언 & input
-변수_가 = int(변수_가)  #자료형 변환
-변수_나 = 0 
-
-
-if(변수_가 == 10): #if문
-    print("값이 10이구나!")
-
-elif(변수_가 == 20): #elif문
-    print("값이 20이구나!")
-
-else: #else문
-    print("값을 모르겠어...")
-
-
-while(변수_나 < 5): #while 문
-    print("반복 횟수 : {}".format(변수_나)) #format 문자
-    변수_나  += 1 #증감 연산
-    break #break문
-
-
-for 연산변수 in [1, 2, 3, 4, 5]: #for 문
-    print("연산변수 값 : {}".format(연산변수))
-
-
-def 더해버리자  (가_변수,  나_변수): #함수 선언
-    return 가_변수 + 나_변수 #값 반환
-
-print("{}".format(더해버리자(1, 2)))
+print("안녕, 세상아!")
 
