2e9803f05ee2ebf1d7db6215128488d7cf241e08
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-09-30 20:10:54 +0900

    src

--- rework_res/main.py
+++ rework_res/main.py
@@ -10,6 +10,9 @@ for line in lineArray:
     linevalue += 1
     result = parsing.code(line, linevalue)
     
+    if(result != None and result < 0):
+        break
+    
 if(result != None):
     if(result < 0):
         SyntaxError.returnErr(result, 0)    
