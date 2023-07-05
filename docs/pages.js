5178cc67712adb29e603fe96746a13cc61d59ff2
BackGwa <backgwa@icloud.com>
2023-07-05 15:22:27 +0900

    문서 업데이트

--- docs/pages.js
+++ docs/pages.js
@@ -1,21 +1,8 @@
 
 const docs = document.getElementById("docs");
-const item = document.querySelectorAll(".document-item");
 
-function switch_page(root, content, index) {
-    unactive();
-    active(index);
+function switch_page(root, content) {
     docs.src = `./pages/${root}/${content}.html`;
 }
 
-function active(index) {
-    item[index - 1].classList.add("focus");
-}
-
-function unactive() {
-    item.forEach(i => {
-        i.classList.remove("focus");
-    });
-}
-
-switch_page('started', 'introduction', 1);
+switch_page('started', 'introduction');
