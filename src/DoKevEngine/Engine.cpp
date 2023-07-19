47237fd2ef11ee26acde609f8c287acc77f4135c
BackGwa <backgwa@icloud.com>
2023-07-19 22:32:41 +0900

    인자 확인 최적화

--- src/DoKevEngine/Engine.cpp
+++ src/DoKevEngine/Engine.cpp
@@ -10,18 +10,18 @@ int inerpreted(std::string Code);
 int compile(std::string FilePath);
 int shell();
 
-
+/* main : DoKevEngine의 시작점입니다. */
 int main(int argc, char* argv[]) {
-    argv_check(argc, argv);
+    if (argc < 2)
+        argv_check(argc, argv);
+    else
+        shell();
+
     return 0;
 }
 
-
 /* argv_check : 옵션 및 인자를 확인합니다. */
 int argv_check(int argc, char* argv[]) {
-    // 엔진 파라미터 확인
-    if (argc < 2) return 0;
-
     // 옵션 및 인자 받기
     std::string option = argv[1];
     std::string argument = argv[2];
@@ -29,21 +29,18 @@ int argv_check(int argc, char* argv[]) {
     if      (option == "-i")    inerpreted(argument);
     else if (option == "-c")    compile(argument);
 
-    return 0;
+    return 1;
 }
-
 
 /* inerpreted : 코드를 인자로 받아 즉시 번역해 실행합니다. */
 int inerpreted(std::string Code) {
     return 0;
 }
 
-
 /* compile : 파일 경로를 인자로 받아 컴파일 후 실행합니다. */
 int compile(std::string FilePath) {
     return 0;
 }
-
 
 /* shell : DoKevEngine의 쉘 인터페이스를 실행합니다. */
 int shell() {
