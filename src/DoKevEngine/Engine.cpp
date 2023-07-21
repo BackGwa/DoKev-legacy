2e6c5d3f6f8a11f5775ba9971725dc970a42c759
BackGwa <backgwa@icloud.com>
2023-07-21 18:11:08 +0900

    알 수 없는 오류 예외처리

--- src/DoKevEngine/Engine.cpp
+++ src/DoKevEngine/Engine.cpp
@@ -3,14 +3,17 @@
 
 #include <iostream>
 #include <string>
+#include "langpack.h"
 #include "shell.h"
 #include "compiler.h"
 #include "debugger.h"
 
+using namespace std;
+
 /* 함수 선언 */
 void argv_isValid(int argc, char *argv[]);
-void inerpreted(std::string Code);
-void compile(std::string FilePath);
+void inerpreted(string Code);
+void compile(string FilePath);
 void shell();
 
 /* main : DoKevEngine의 시작점입니다. */
@@ -25,26 +28,42 @@ int main(int argc, char *argv[]) {
 
 /* argv_isValid : 옵션 및 인자를 확인합니다. */
 void argv_isValid(int argc, char *argv[]) {
-  std::string option = argv[1];     // 옵션 가져오기
-  std::string argument = argv[2];   // 인자 가져오기
+  string option = argv[1];     // 옵션 가져오기
+  string argument = argv[2];   // 인자 가져오기
 
   if (option == "-i")
     inerpreted(argument);
   else if (option == "-c")
     compile(argument);
+  else {
+    string TARGET = "";
+
+    for (int i = 0; i < argc; i++) {
+      TARGET = TARGET + " " + argv[i];
+    }
+
+    StandardError(0,
+                  UNKNOWN_OPTION_TITLE,
+                  UNKNOWN_OPTION_MESSAGE,
+                  TARGET,
+                  option,
+                  UNKNOWN_OPTION_SUGGESTION,
+                  UNKNOWN_OPTION_SUGGESTION_CONTENT,
+                  2);
+  }
 }
 
 /* inerpreted : 코드를 인자로 받아 즉시 번역해 실행합니다. */
-void inerpreted(std::string Code) {
-  std::cout << "인터프리트 실행" << std::endl;
+void inerpreted(string code) {
+  cout << "인터프리트 실행" << endl;
 }
 
 /* compile : 파일 경로를 인자로 받아 컴파일 후 실행합니다. */
-void compile(std::string FilePath) {
-  std::cout << "컴파일 실행" << std::endl;
+void compile(string file_path) {
+  cout << "컴파일 실행" << endl;
 }
 
 /* shell : DoKevEngine의 쉘 인터페이스를 실행합니다. */
 void shell() {
-  std::cout << "쉘 인터페이스 실행" << std::endl;
+  cout << "쉘 인터페이스 실행" << endl;
 }
