771846df22b0c412ae2d5f733f6932b8530f5ddf
BackGwa <backgwa@icloud.com>
2023-07-21 18:50:47 +0900

    Revert "Merge pull request #8 from BackGwa/develop"
    This reverts commit 11ca3610feb5582549f282ceb4d511fbfb5f672d, reversing
    changes made to 310656032016df2275c51e3c931a26f4ce38dde4.

---
+++ README.md
@@ -0,0 +1,26 @@
+<br>
+
+<div align="center">
+  <img src="./docs/res/banner.png" width='300'>
+</div>
+
+<br>
+
+---
+
+<br>
+
+## **DoKev 배우기**
+다음 <a href = 'https://backgwa.notion.site/DoKev-46bc63939be74aa28d3247ed2ec8d415'>링크</a>에서 DoKev에 대한 거의 모든 내용을 볼 수 있어요!
+
+<br>
+
+## **DoKev 예제**
+다음 <a href ='https://github.com/BackGwa/DoKev-exam'>링크</a>에서 DoKev 예제를 볼 수 있어요!
+
+---
+
+<br>
+
+## **커피 사주기**
+토스뱅크 1908-8294-2426
---
+++ src/compiler/Program.cs
@@ -0,0 +1,179 @@
+﻿using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace DoKevEngine {
+
+    partial class Program {
+        static void Main() {
+
+            /* 빌더 화면 구성 */
+            Console.Clear();
+            Console.Title = "DoKev Runner";
+
+            /* 클래스 연결 */
+            RichSupport rich = new RichSupport();
+            Config cf = new Config();
+
+            cf.ConfigSet();
+
+            rich.Log(cf.Text("info", "builder"), (cf.Cfg("builder", "version")));
+
+            var locale = cf.Text;
+            var cfg = cf.Cfg;
+            var log = rich.Log;
+            var NowTime = rich.NowTime;
+
+            /* 시스템 아키텍쳐와 운영체제 정보 선언 */
+            var ARCH = RuntimeInformation.ProcessArchitecture;
+            var OS = Environment.OSVersion.Platform.ToString();
+
+            log(locale("info", "system"), $"{OS} ({ARCH})");
+            log(locale("info", "lang"), cfg("builder", "language"));
+
+            /* baseDirectory에 빌드 툴의 절대 경로 선언 */
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            log($"\n{locale("info", "path")}", baseDirectory);
+
+            /* 빌드 파일 폴더 유무 확인 */
+            string exportfolder = AppDomain.CurrentDomain.BaseDirectory + $"{cfg("folder", "export")}";
+            DirectoryInfo ef = new DirectoryInfo(exportfolder);
+            if (!ef.Exists) {
+                log($"\n{locale("createfolder", "title")}", locale("createfolder", "invalid"), "warning");
+                try {
+                    ef.Create();
+                    log($"{locale("createfolder", "title")}", $"{locale("createfolder", "create")}\n", "success");
+                } catch {
+                    log($"{locale("createfolder", "title")}", $"{locale("createfolder", "error")}\n", "fatal");
+                }
+            }
+
+            /* filename 설정 및 선언 */
+            string targetfile = cfg("filename", "target");
+            string exportfile = cfg("filename", "export");
+
+            /* 변환에 필요한 변수 선언 */
+            bool Enable_random = false, Enable_os = false;
+            string[] WriteLine = new string[1];
+            string filePath;
+
+            /* targetfile 유효성 확인을 위한 경로 선언 */
+            try {
+                filePath = Path.Combine(baseDirectory, targetfile);
+            } catch {
+                log(locale("target", "error"), "fatal");
+                return;
+            }
+
+            /* targetfile 유효성 확인 */
+            if (File.Exists(filePath)) {
+                log(locale("target", "isvalid"), $"{targetfile} {locale("target", "valid")}", "success", true);
+                log(NowTime(), $"{locale("build", "start")}\n");
+                Converter();
+            } else {
+                log(locale("target", "isvalid"), $"{targetfile} {locale("target", "invalid")}", "fatal");
+                return;
+            }
+
+            /* Converter
+             * 설정 된 dkv 파일을 Python 코드로 빌드합니다. */
+            void Converter() {
+                log(NowTime(), locale("build", "initializing"));
+
+                /* 파싱에 필요한 변수 선언 */
+                Parser parser = new Parser();
+  
+                log(NowTime(), $"{locale("build", "initialized")}", "success");
+
+                /* 코드를 파싱하여 변환 */
+                foreach (string code in File.ReadLines($"{baseDirectory}/{targetfile}")) {
+
+                    /* 코드가 공백이면, 파서 실행 패스 */
+                    if (code.Replace(" ", "") == "") continue;
+
+                    /* 라이브러리 확인 */
+                    if (parser.LIBCHK(code)) {
+                        switch (parser.NAME_FIX(parser.LIBPARSER(code))) {
+                            case "random":
+                                Enable_random = true;   break;
+                            case "os":
+                                Enable_os = true;       break;
+                            case "":
+                                                        return;
+                            default:
+                                rich.SyntaxWarning(code, "unknown-lib");
+                                break;
+                        }
+                    }
+
+                    /* 파서 실행 */
+                    string Result = code;
+
+                    /* 변환 대기열 */
+                    log($"\n{NowTime()}", Result.Replace("    ", ""));
+
+                    /* 내장 함수 파싱 작업 */
+                    if (Enable_os)      Result = parser.OS_PARSER(Result);
+                    if (Enable_random)  Result = parser.RANDOM_PARSER(Result);
+                                        Result = parser.PARSER(Result);
+
+                    if (Result == "")   return;
+
+                    log(NowTime(), Result.Replace("    ", ""));
+
+                    /* 파싱 된 코드 작성 */
+                    WriteLine[WriteLine.Length - 1] = Result;
+                    Array.Resize(ref WriteLine, WriteLine.Length + 1);
+
+                }
+
+                log($"\n{NowTime()}", locale("build", "finish"), "success", true);
+
+                /* 빌드 파일 작성 */
+                log(NowTime(), locale("file", "ready"));
+                StreamWriter writer;
+
+                try {
+                    writer = File.CreateText($"{baseDirectory}/{cfg("folder", "export")}/{exportfile}");
+                    foreach (string Code in WriteLine) writer.WriteLine(Code);
+                    writer.Close();
+                    log(NowTime(), locale("file", "finish"), "success", true);
+                } catch {
+                    log(NowTime(), locale("file", "error"), "fatal");
+                    return;
+                }
+
+                /* 디버깅 시작 */
+                try {
+                    log(NowTime(), locale("debug", "start"), "default", true);
+                    Runner();
+                } catch {
+                    log($"\n{NowTime()}", locale("debug", "error"), "fatal");
+                    return;
+                }
+            }
+
+
+            /* Runner
+             * 빌드된 파일을 설정 인터프리터 환경에 맞게 실행합니다. */
+            void Runner() {
+                Process module = new Process();
+
+                string interpreterPath = cfg("interpreter", "custom") == "false" ? (OS == "Unix" ? "python3" : "python") : cfg("interpreter", "path");
+                string interpreterArgs = cfg("interpreter", "custom") == "false" ? $"-d \"{baseDirectory}/{cfg("folder", "export")}/{exportfile}\"" : $"{cfg("interpreter", "arguments")} \"{baseDirectory}/{cfg("folder", "export")}/{exportfile}\"";
+
+                module.StartInfo = new ProcessStartInfo {
+                    FileName = interpreterPath,
+                    Arguments = interpreterArgs
+                };
+
+                module.Start();
+                module.WaitForExit();
+                Console.ReadKey();
+            }
+
+        }   /* Main Function */
+
+    }       /* Program Class */
+
+}           /* DoKevEngine namespace */
