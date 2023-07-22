bd5019f611b1e8df0b6af5e0feaa55ac68419bf4
BackGwa <backgwa@icloud.com>
2023-07-22 22:40:59 +0900

    입력 오류 수정

--- src/DoKevEngine/shell.hpp
+++ src/DoKevEngine/shell.hpp
@@ -2,18 +2,19 @@
 #include "compiler.hpp"
 #include "debugger.hpp"
 #include "langpack.hpp"
+#include "shell_interpreter.hpp"
 
 using namespace std;
 
 /* Shell_open : 쉘 인터페이스 환경을 엽니다. */ 
 void Shell_open() {
-  string command = "";
-
+  char command[128];
   cout << "DoKev" << VERSION << " - " << BUILD << endl << endl;
 
   while(true) {
     cout << ">>> ";
-    cin >> command;
-    cout << command;
+    cin.getline(command, 128);
+
+    command_check(command);
   }
 }
