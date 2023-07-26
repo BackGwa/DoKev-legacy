9ab93a087bc16e7a7312a9d7980310649f939bd1
BackGwa <backgwa@icloud.com>
2023-07-26 21:11:45 +0900

    쉘 버전 정보 볼드 처리

--- src/DoKevEngine/shell.hpp
+++ src/DoKevEngine/shell.hpp
@@ -9,7 +9,7 @@ using namespace std;
 /* Shell_open : 쉘 인터페이스 환경을 엽니다. */ 
 void Shell_open() {
   char command[128];
-  cout << "DoKev" << VERSION << " | " << SHELL_VERSION << BUILD << endl;
+  cout << BOLD << "DoKev" << VERSION << " | " << SHELL_VERSION << BUILD << RESET << endl;
   cout << SHELL_MESSAGE << endl << endl;
 
   while(true) {
