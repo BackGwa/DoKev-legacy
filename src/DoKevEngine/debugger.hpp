65a66e407bd855aa780eeeaceef3803d4073b856
BackGwa <backgwa@icloud.com>
2023-07-26 16:23:00 +0900

    SyntaxError 함수 추가

--- src/DoKevEngine/debugger.hpp
+++ src/DoKevEngine/debugger.hpp
@@ -17,25 +17,56 @@ void StandardError(int LINE,
                   int len = 1
                   ) {
 
-  cout << endl << RED << ERROR << RESET << TITLE << endl;
-  line_counter(LINE, false);
-  cout << endl;
+  cout << endl << RED << BOLD << ERROR << RESET << BOLD << TITLE << RESET << endl;
+  line_counter(LINE, false, true);
   line_counter(LINE);
   cout << TARGET << endl;
 
-  highlighter(LINE, TARGET, HIGHLIGHT);
+  highlighter(LINE, TARGET, HIGHLIGHT, MESSAGE);
 
-  cout << " " << RED << MESSAGE << RESET << endl << endl;
+  cout << CYAN << BOLD << HELP << RESET << BOLD << SUGGESTION << RESET << endl;
 
-  cout << CYAN << HELP << RESET << SUGGESTION << endl;
-
-  line_counter(LINE, false);
-  cout << endl;
+  line_counter(LINE, false, true);
 
   for (int i = 0; i < len; i++) {
     line_counter(LINE, false);
-    cout << GREEN << SUGGESTION_CONTENT[i].first << RESET << " : ";
+    cout << GREEN << BOLD << SUGGESTION_CONTENT[i].first << ": " << RESET;
     cout << SUGGESTION_CONTENT[i].second << endl;
   }
+  exit(0);
+}
+
+/* SyntaxError : 코드의 문법적 에러를 출력합니다. */
+void SyntaxError(int LINE,
+                string TITLE,
+                string MESSAGE,
+                string TARGET,
+                string HIGHLIGHT,
+                const std::vector<std::tuple<std::string, std::string, bool>>& SUGGESTION_CONTENT,
+                int len = 1
+                ) {
+
+  cout << endl << RED << BOLD << ERROR << RESET << BOLD << TITLE << endl;
+  line_counter(LINE, false, true);
+  line_counter(LINE);
+  cout << TARGET << endl;
+
+  highlighter(LINE, TARGET, HIGHLIGHT, MESSAGE);
+
+  cout << CYAN << BOLD << HELP << RESET << BOLD << SYNTAX_ERROR << RESET << endl;
+
+  for (int i = 0; i < len; i++) {
+    const string SUGGESTION_CODE = get<0>(SUGGESTION_CONTENT[i]);
+    const string SUGGESTION_CODE_MESSAGE = get<1>(SUGGESTION_CONTENT[i]);
+    const string SAFE = (get<2>(SUGGESTION_CONTENT[i]) ? GREEN : RED);
+
+    if (i != 0) cout << endl;
+    line_counter(LINE, false, true);
+    line_counter(LINE);
+    cout << SUGGESTION_CODE << endl;
+    line_counter(LINE, false);
+    cout << SAFE << BOLD << "~ " << SUGGESTION_CODE_MESSAGE << RESET << endl;
+  }
+
   exit(0);
 }
