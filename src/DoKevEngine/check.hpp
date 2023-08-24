8a26975fa79fdbcfe0d797ceee004e72fb481e73
BackGwa <backgwa@icloud.com>
2023-08-24 14:58:18 +0900

    #9 헤더 파일 오류 수정

--- src/DoKevEngine/check.hpp
+++ src/DoKevEngine/check.hpp
@@ -1,14 +1,24 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+
+#ifdef _WIN32
+#include <io.h>
+#else
 #include <unistd.h>
+#endif
 
 using namespace std;
 
 bool filecheck(string path) {
-  return (access(path.c_str(), F_OK) == 0);
+#ifdef _WIN32
+    return (_access(path.c_str(), 0) == 0);
+#else
+    return (access(path.c_str(), F_OK) == 0);
+#endif
 }
 
 bool blankcheck(string argument) {
-  return argument == "";
+    return argument.empty();
 }
