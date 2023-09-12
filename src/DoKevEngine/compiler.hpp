b86502ba3bab6aca5c2355c0a34b24665dbc6961
BackGwa <backgwa@icloud.com>
2023-09-12 09:32:34 +0900

    출력문 추가 구현

--- src/DoKevEngine/compiler.hpp
+++ src/DoKevEngine/compiler.hpp
@@ -24,13 +24,13 @@ void openfile(string filepath, string TARGET, string MAKER) {
             TARGET,
             MAKER,
             RECHECKING,
-            FILE_OPEN_ERROR_CONTENT,
+            FILE_OPEN_ERROR_SUGGESTION_CONTENT,
             FILE_OPEN_ERROR_INDEX);
     } else {
         // 파일을 읽어 codelist에 저장
         string line;
         while (std::getline(file, line)) {
-            if(line != "") codelist.push_back(line);
+            codelist.push_back(line);
         }
         file.close();
     }
@@ -68,29 +68,110 @@ string COMMENT(string line) {
     return result;
 }
 
-
-/* PRINT : 출력문인지, 검사하고 변경합니다. */
-string PRINT(string line) {
-    regex pattern("\"([^\"]*)\"|'([^']*)'|말해줘");
+/* PARTICAL : 조사인지, 검사하고 변경합니다. */
+string PARTICAL_TOKEN(string line) {
+    regex pattern("\"([^\"]*)\"|'([^']*)'|이라고 |라고 |을 |를 ");
     smatch matches;
     string result;
 
-    // 문자열 처리
     auto it = line.cbegin();
     
     while (regex_search(it, line.cend(), matches, pattern)) {
+        const string match = matches[0];
         result += matches.prefix();
-        if (matches[0] == "말해줘")
-            result += "$(PRINT)";
+
+        if (match == "이라고 " || match == "라고 " || match == "을 " || match == "를 " )
+            result += "->[particle]->";
+        else
+            result += matches[0];
+
+        it = matches[0].second;
+    }
+
+    result += string(it, line.cend());
+    return result;
+}
+
+/* VERB : 동사인지, 검사하고 변경합니다. */
+string VERB_TOKEN(string line) {
+    regex pattern("\"([^\"]*)\"|'([^']*)'|줘|하고|해주고|주고|고");
+    smatch matches;
+    string result;
+
+    auto it = line.cbegin();
+    
+    while (regex_search(it, line.cend(), matches, pattern)) {
+        const string match = matches[0];
+        result += matches.prefix();
+
+        if (match == "줘" || match == "하고" || match == "해주고" || match == "주고" || match == "고" )
+            result += "[verb]";
+        else
+            result += matches[0];
+
+        it = matches[0].second;
+    }
+
+    result += string(it, line.cend());
+    return result;
+}
+
+string PRINT_TOKEN(string line) {
+    regex pattern("\"([^\"]*)\"|'([^']*)'|말해|보여|말한|출력|말하");
+    smatch matches;
+    string result;
+
+    auto it = line.cbegin();
+    
+    while (regex_search(it, line.cend(), matches, pattern)) {
+        const string match = matches[0];
+        result += matches.prefix();
+
+        if (match == "말해" || match == "보여" || match == "말한" || match == "출력" || match == "말하" )
+            result += "[print]->";
         else
             result += matches[0];
         it = matches[0].second;
     }
 
-    // 나머지 추가
-    result += std::string(it, line.cend());
-
+    result += string(it, line.cend());
     return result;
+}
+
+/* PRINT : 출력문인지, 검사하고 변경합니다. */
+string PRINT(string line) {
+    line = PARTICAL_TOKEN(line);
+    line = VERB_TOKEN(line);
+    line = PRINT_TOKEN(line);
+
+    // 출력문이 아니면, 파서 종료
+    if(!line.contains("[print]->")) {
+        return line;
+    }
+
+    // 조사가 없으면, 오류 발생
+    if(!line.contains("->[particle]->"))
+        SyntaxError(line_number + 1,
+            NOT_CONTAIN_PARTICLE_TITLE,
+            NOT_CONTAIN_PARTICLE_MESSAGE,
+            before_code,
+            "\"",
+            NOT_CONTAIN_PARTICLE_SUGGESTION_CONTENT,
+            NOT_CONTAIN_PARTICLE_INDEX
+        );
+
+    // 동사가 없으면, 오류 발생
+    if(!line.contains("[verb]"))
+        SyntaxError(line_number + 1,
+            NOT_CONTAIN_VERB_TITLE,
+            NOT_CONTAIN_VERB_MESSAGE,
+            before_code,
+            "EOW",
+            NOT_CONTAIN_VERB_SUGGESTION_CONTENT,
+            NOT_CONTAIN_VERB_INDEX
+        );
+
+    return line;
 }
 
 /* parsing : 코드를 확인, 변경하고, 검사합니다. */
