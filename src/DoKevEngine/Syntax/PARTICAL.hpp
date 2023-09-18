7b0f8ba648a7236ec4e0e3c33b207128ffec76da
BackGwa <backgwa@icloud.com>
2023-09-18 19:38:38 +0900

    대입연산 조사 추가

--- src/DoKevEngine/Syntax/PARTICAL.hpp
+++ src/DoKevEngine/Syntax/PARTICAL.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-/* PARTICAL : 조사인지, 검사하고 변경합니다. */
+/* PARTICAL_TOKEN : 조사인지, 검사하고 변경합니다. */
 string PARTICAL_TOKEN(string line) {
     regex pattern("\"([^\"]*)\"|'([^']*)'|이라고 |라고 |을 |를 ");
     smatch matches;
@@ -14,6 +14,30 @@ string PARTICAL_TOKEN(string line) {
 
         if (match == "이라고 " || match == "라고 " || match == "을 " || match == "를 ")
             result += "<-particle->";
+        else
+            result += matches[0];
+
+        it = matches[0].second;
+    }
+
+    result += string(it, line.cend());
+    return result;
+}
+
+/* ASSIGNMENT_OPERATOR : 대입연산 조사인지, 검사하고 변경합니다. */
+string ASSIGNMENT_OPERATOR(string line) {
+    regex pattern("\"([^\"]*)\"|'([^']*)'|은 |는 ");
+    smatch matches;
+    string result;
+
+    auto it = line.cbegin();
+
+    while (regex_search(it, line.cend(), matches, pattern)) {
+        const string match = matches[0];
+        result += matches.prefix();
+
+        if (match == "은 " || match == "는 ")
+            result += " = ";
         else
             result += matches[0];
 
