fb595927100e43841f80cf1634723dbab7c06f9f
BackGwa <backgwa@icloud.com>
2023-09-19 15:13:54 +0900

    변수 지원 헤더로 이동

--- src/DoKevEngine/Syntax/PARTICAL.hpp
+++ src/DoKevEngine/Syntax/PARTICAL.hpp
@@ -23,27 +23,3 @@ string PARTICAL_TOKEN(string line) {
     result += string(it, line.cend());
     return result;
 }
-
-/* ASSIGNMENT_OPERATOR : 대입연산 조사인지, 검사하고 변경합니다. */
-string ASSIGNMENT_OPERATOR(string line) {
-    regex pattern("\"([^\"]*)\"|'([^']*)'|은 |는 ");
-    smatch matches;
-    string result;
-
-    auto it = line.cbegin();
-
-    while (regex_search(it, line.cend(), matches, pattern)) {
-        const string match = matches[0];
-        result += matches.prefix();
-
-        if (match == "은 " || match == "는 ")
-            result += " = ";
-        else
-            result += matches[0];
-
-        it = matches[0].second;
-    }
-
-    result += string(it, line.cend());
-    return result;
-}
