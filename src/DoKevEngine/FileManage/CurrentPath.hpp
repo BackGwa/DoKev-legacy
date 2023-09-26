0a69bf01f694f52d52553da1f4223d1823932284
BackGwa <backgwa@icloud.com>
2023-09-26 14:16:10 +0900

    CurrentPath 헤더 최적화

--- src/DoKevEngine/FileManage/CurrentPath.hpp
+++ src/DoKevEngine/FileManage/CurrentPath.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
 /* CurrentPath : 현재 경로를 반환합니다. */
-string CurrentPath() {
-    return filesystem::current_path().string();
+std::string CurrentPath() {
+    return std::filesystem::current_path().string();
 }
