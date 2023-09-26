2b0fa54f201602cca022ef6014b259d474aa610a
BackGwa <backgwa@icloud.com>
2023-09-26 15:06:11 +0900

    내장된 String 헤더 파일들 최적화

--- src/DoKevEngine/String/ToMultiByte.hpp
+++ src/DoKevEngine/String/ToMultiByte.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-using namespace std;
-
 /* ToMultiByte : 입력받은 wstring 문자열을 string 문자열로 변환합니다. */
-string ToMultiByte(const std::wstring& input) {
+std::string ToMultiByte(const std::wstring& input) {
     std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
     return converter.to_bytes(input);
 }
--- src/DoKevEngine/String/ToUnicode.hpp
+++ src/DoKevEngine/String/ToUnicode.hpp
@@ -2,10 +2,8 @@
 
 #include <codecvt>
 
-using namespace std;
-
 /* ToUnicode : 입력받은 string 문자열을 wstring 문자열로 변환합니다. */
-wstring ToUnicode(const string &str) {
+wstring ToUnicode(const std::string &str) {
     wstring_convert<codecvt_utf8<wchar_t>> converter;
     return converter.from_bytes(str);
 }
