b5fe3d97d0961859d29f09b4a93214f641d2a51d
BackGwa <backgwa@icloud.com>
2023-09-26 14:11:40 +0900

    IsKorean 헤더 함수 최적화

--- src/DoKevEngine/Check/IsKorean.hpp
+++ src/DoKevEngine/Check/IsKorean.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 /* IsKorean : 입력 된 문자열이 한글인지 아닌지 확인합니다. */
-bool IsKorean(const string& str) {
-    regex korean("[가-힣]+");
-    return regex_search(str, korean);
+bool IsKorean(const std::string& str) {
+    static const std::regex korean("[가-힣]+");
+    return std::regex_search(str, korean);
 }
