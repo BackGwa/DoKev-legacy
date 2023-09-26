bd4791812347caf71d36a335f97585b6b91f1bba
BackGwa <backgwa@icloud.com>
2023-09-26 14:11:47 +0900

    Valid 헤더 함수 최적화

--- src/DoKevEngine/Check/Valid.hpp
+++ src/DoKevEngine/Check/Valid.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-using namespace std;
-
 /* Valid : 문자열의 구조가 같은지 비교합니다. */
-bool Valid(const string line, const string syntax) {
-    return line.find(syntax) != string::npos;
+bool Valid(const std::string& line, const std::string& syntax) {
+    return line.find(syntax) != std::string::npos;
 }
