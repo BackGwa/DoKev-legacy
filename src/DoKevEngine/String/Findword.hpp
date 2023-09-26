845aeeacf13c17a1b265e491376348288cdd88a1
BackGwa <backgwa@icloud.com>
2023-09-26 14:25:40 +0900

    헤더 최적화 : `Findword.hpp`

--- src/DoKevEngine/String/Findword.hpp
+++ src/DoKevEngine/String/Findword.hpp
@@ -1,11 +1,9 @@
 #pragma once
 
-using namespace std;
-
 /* Findword : 전체 문자열에서 특정 문자열을 찾아 인덱스를 반환합니다. */
-int Findword(const string code, const string word) {
+int Findword(const std::string code, const std::string word) {
     size_t found = ToUnicode(code).rfind(ToUnicode(word));
-    if (found != wstring::npos) {
+    if (found != std::wstring::npos) {
         return static_cast<int>(found);
     }
     return -1;
