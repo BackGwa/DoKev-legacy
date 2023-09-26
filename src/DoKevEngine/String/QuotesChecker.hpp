083cadfbfb9989c9d444d964377c1cb6316d3256
BackGwa <backgwa@icloud.com>
2023-09-26 14:40:31 +0900

    헤더 최적화 : `QuotesChecker.hpp`

--- src/DoKevEngine/String/QuotesChecker.hpp
+++ src/DoKevEngine/String/QuotesChecker.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
 /* QuotesChecker : 정상적인, 문자열인지 검사합니다. */
-bool QuotesChecker(string code) {
+bool QuotesChecker(const std::string& code) {
     int singleQuoteCount = 0;
     int doubleQuoteCount = 0;
 
     for (char c : code) {
-        if (c == '\'')      singleQuoteCount++;
-        else if (c == '"')  doubleQuoteCount++;
+        if (c == '\'') {
+            singleQuoteCount++;
+        } else if (c == '"') {
+            doubleQuoteCount++;
+        }
     }
 
-    if(singleQuoteCount + doubleQuoteCount % 2 != 0) {
-        return false;
-    }
-    return true;
+    return (singleQuoteCount % 2 == 0) && (doubleQuoteCount % 2 == 0);
 }
