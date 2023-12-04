065d8c8e0c9ad27a1e7ba215861b4a98c3bd10f3
BackGwa <backgwa@icloud.com>
2023-12-04 20:08:37 +0900

    조건문 구현

--- src/DoKevEngine/Syntax/IFTHEN.hpp
+++ src/DoKevEngine/Syntax/IFTHEN.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
+#include "../String/BlankRemove.hpp"
 
 /* IF_TARGRT : 조건문의 값 대상을 토큰화합니다. */
 string IF_TARGET(string line) {
-    regex pattern("\"([^\"]*)\"|'([^']*)'|랑 |가 |보다 |와 |이 ");
+    regex pattern("\"([^\"]*)\"|'([^']*)'|이랑 |랑 |가 |보다 |와 |과 |이 ");
     smatch matches;
     string result;
 
@@ -13,9 +14,9 @@ string IF_TARGET(string line) {
         const string match = matches[0];
         result += matches.prefix();
 
-        if (match == "랑 ")
+        if (match == "이랑 " || match == "랑 ")
             result += "<-first-value->";
-        else if (match == "보다 " || match == "와 ")
+        else if (match == "보다 " || match == "와 " || match == "과 ")
             result += "<-first-value->";
         else if (match == "이 " || match == "가 ")
             result += "<-second-value->";
@@ -27,40 +28,6 @@ string IF_TARGET(string line) {
 
     result += string(it, line.cend());
 
-    return result;
-}
-
-/* IF_NORMAL : 조건문을 토큰화합니다. */
-string IF_NORMAL(string line) {
-    regex pattern("\"([^\"]*)\"|'([^']*)'|크다면|작다면|같다면|같지않다면");
-    smatch matches;
-    string result;
-
-    auto it = line.cbegin();
-
-    while (regex_search(it, line.cend(), matches, pattern)) {
-        const string match = matches[0];
-        result += matches.prefix();
-
-        if (match == "크다면")
-            result += "<-big-then->";
-        else if (match == "작다면")
-            result += "<-small-then->";
-        else if (match == "같다면")
-            result += "<-equal-then->";
-        else if (match == "같지않다면")
-            result += "<-not-equal-then->";
-        else
-            result += matches[0];
-
-        it = matches[0].second;
-    }
-
-    result += string(it, line.cend());
-
-    if (result != line)
-        result = IF_TARGET(result);
-    
     return result;
 }
 
@@ -124,14 +91,125 @@ string IF_AND(string line) {
     return result;
 }
 
+/* IF_PARSING : 조건문을 파싱하여 변경합니다. */
+string IF_PARSING(string line) {
+    string result = "";
+    
+    vector<string> T = Split(line, "->");
+    vector<string> R;
+    vector<string> P;
+
+    int condition_len = T.size();
+
+    for (int arr_len = 0; condition_len > arr_len; arr_len++) {
+        R = Split(T[arr_len], "<-");
+        string value;
+
+        if (arr_len != 0)
+            value = ToMultiByte(Substring(ToUnicode(R[0]), 1, sizeof(ToUnicode(R[0]))));
+        else
+            value = R[0];
+
+        string temp = value;
+        BlankRemove(temp);
+
+        if (!temp.empty())
+            P.push_back(value);
+        P.push_back(R[1]);
+    }
+
+    condition_len = P.size();
+
+    for (int arr_len = 0; condition_len > arr_len; arr_len++) {
+        string first = "";
+        string second = "";
+        string condition = "";
+        
+        if (P[arr_len] == "-first-value") {
+            first = P[arr_len - 1];
+            second = P[arr_len + 1];
+
+            condition = P[arr_len + 3];
+
+            if (condition == "-big-or")
+                condition = first + " < " + second + " or ";
+
+            else if (condition == "-small-or")
+                condition = first + " > " + second + " or ";
+
+            else if (condition == "-equal-or")
+                condition = first + " == " + second + " or ";
+
+            else if (condition == "-not-equal-or")
+                condition = first + " != " + second + " or ";
+
+            else if (condition == "-big-and")
+                condition = first + " < " + second + " and ";
+
+            else if (condition == "-small-and")
+                condition = first + " > " + second + " and ";
+
+            else if (condition == "-equal-and")
+                condition = first + " == " + second + " and ";
+
+            else if (condition == "-not-equal-and")
+                condition = first + " != " + second + " and ";
+
+            else if (condition == "-big-then")
+                condition = first + " < " + second;
+
+            else if (condition == "-small-then")
+                condition = first + " > " + second;
+
+            else if (condition == "-equal-then")
+                condition = first + " == " + second;
+
+            else if (condition == "-not-equal-then")
+                condition = first + " != " + second;
+
+            result += condition;
+        }
+
+    }
+
+    result = "if (" + result + "):";
+    return result;
+}
+
 /* IFTHEN : 조건문을 검사하고 변경합니다. */
 string IFTHEN(string line) {
-    string cache = line;
+    regex pattern("\"([^\"]*)\"|'([^']*)'|크다면|작다면|같다면|같지않다면");
+    smatch matches;
+    string result;
 
-    cache = IF_NORMAL(cache);
-    cache = IF_AND(cache);
-    cache = IF_OR(cache);
-    cout << GREEN << IF_OR(cache) << RESET << endl;
+    auto it = line.cbegin();
 
-    return line;
+    while (regex_search(it, line.cend(), matches, pattern)) {
+        const string match = matches[0];
+        result += matches.prefix();
+
+        if (match == "크다면")
+            result += "<-big-then->";
+        else if (match == "작다면")
+            result += "<-small-then->";
+        else if (match == "같다면")
+            result += "<-equal-then->";
+        else if (match == "같지않다면")
+            result += "<-not-equal-then->";
+        else
+            result += matches[0];
+
+        it = matches[0].second;
+    }
+
+    result += string(it, line.cend());
+
+    if (result != line) {
+        result = IF_TARGET(result);
+        result = IF_AND(result);
+        result = IF_OR(result);
+        result = IF_PARSING(result);
+    }
+    
+    return result;
 }
