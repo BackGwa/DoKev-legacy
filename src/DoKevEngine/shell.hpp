d43f1d6bfbf20b146fa6c95662a9a9ccacdc82ed
BackGwa <backgwa@icloud.com>
2023-12-04 16:51:08 +0900

    코드 시인성 개선

--- src/DoKevEngine/shell.hpp
+++ src/DoKevEngine/shell.hpp
@@ -9,16 +9,18 @@ void Shell_open() {
   cout << BOLD << "DoKev" << VERSION << " | " << SHELL_VERSION << BUILD << RESET << endl;
   cout << SHELL_MESSAGE << endl << endl;
 
-  while(true) {
+  while (true) {
     cout << BOLD << "⟩⟩⟩ " << RESET;
     cin.getline(command, 128);
 
     string str_command(command);
 
     // 쉘 인터페이스 exit() 방지
-    if(Valid(str_command, "exit()")) exit(0);
-    
-    parsing(line, command, true);
-    line++;
+    if (Valid(str_command, "exit()")) {
+      exit(0);
+    } else {
+      parsing(line, command, true);
+      line++;
+    }
   }
 }
